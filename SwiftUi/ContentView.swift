4184b80ba680420578b1d8658f13969cabdf2cae
kajumac07 <kajumac13@gmail.com>
2025-09-06 18:38:39 +0530

    DetailsImageView created and fixed some design issue

--- SwiftUi/ContentView.swift
+++ SwiftUi/ContentView.swift
@@ -1,15 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @ObservedObject var imageViewModel: ImageViewModel
-    
+
     var body: some View {
         NavigationView {
             List(imageViewModel.imageModel) { model in
-                VStack {
-                    MyImageView(url: model.downloadUrl ?? "")
-                    Text(model.author ?? "").font(.system(size: 20, weight: .bold, design: .rounded))
+                NavigationLink(
+                    destination: DetailView(url:model.downloadUrl ?? "" )
+                ) {
+                    VStack {
+                        MyImageView(
+                            url: model.downloadUrl ?? "",
+                            isDetailView: false
+                        )
+                        Text(model.author ?? "").font(
+                            .system(size: 20, weight: .bold, design: .rounded)
+                        )
+                    }
                 }
             }
             .onAppear(perform: imageViewModel.loadData)
@@ -24,15 +33,24 @@ struct ContentView: View {
 
 struct MyImageView: View {
     var url: String
+    var isDetailView: Bool
     var body: some View {
         AsyncImage(url: URL(string: url)) { image in
-            image
-                .resizable()
-                .scaledToFit()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(20)
+            image.resizable()
+                .aspectRatio(contentMode: isDetailView ? .fill : .fit)
+                .cornerRadius(isDetailView ? 0 : 20)
         } placeholder: {
             ProgressView()
         }
+    }
+}
+
+struct DetailView: View {
+    var url: String
+    var body: some View {
+        MyImageView(
+            url: url,
+            isDetailView: true
+        ).ignoresSafeArea()
     }
 }
