74a0a9e680be56aaeefa1e412919d1e078a64042
kajumac07 <kajumac13@gmail.com>
2025-09-06 16:32:34 +0530

    Initial Commit

---
+++ SwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi
+//
+//  Created by Atul Tiwari on 06/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi/SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiApp.swift
+//  SwiftUi
+//
+//  Created by Atul Tiwari on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
